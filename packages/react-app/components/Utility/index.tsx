d826b7330a1bec08ab248b66fd53f90eb9b27891
damzylance <wasiuadegoke14@gmail.com>
2024-06-17 08:23:43 +0100

    disable paybill

--- packages/react-app/components/Utility/index.tsx
+++ packages/react-app/components/Utility/index.tsx
@@ -248,7 +248,8 @@ const handleCountryChange = (e:any)=>{
         </>}
         
          {
-         userCountry==="KE" &&   <><UtilityCard
+         userCountry==="KE" &&   <>
+         {/* <UtilityCard
           bg={"linear-gradient(87.4deg, rgba(255, 123, 123, 0.35) 0%, rgba(123, 0, 255, 0.35) 100%)"}
             icon={<MdPayment />}
             text={"Pay Bills"}
@@ -257,7 +258,7 @@ const handleCountryChange = (e:any)=>{
               onOpen();
             }}
             
-          />
+          /> */}
           <UtilityCard
           bg={"linear-gradient(87.4deg, rgba(0, 255, 204, 0.35) 0%, rgba(0, 51, 204, 0.35) 100%)"}
             icon={<MdPayment />}
